70336e4d90fd64a9e09d8d86cdc34716822b969f
lvlinu <s01085056889@gmail.com>
2023-09-07 15:43:40 +0900

    ppp

--- par/FileName.cpp
+++ par/FileName.cpp
@@ -1,10 +1,27 @@
 #include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 
-void main() {
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int num;
 
-	printf("Hello KNU!\n");
-	printf("Hello KNU!\n");
-	printf("Hello KNU!\n");
-	
+	printf("n의 숫자를 입력하여 피라미드 만들기 : ");
+	scanf_s("%d", &num);
 
+	for (a = 0; a < num; a++)
+	{
+		for (b = a; b <= num - 1; b++) {
+			printf(" ");
+		}
+		for (c = 0; c <= (a * 2); c++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+	return 0;
 }
