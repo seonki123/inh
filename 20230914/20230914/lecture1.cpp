c1e12a268b970e670e1346cb9c8124908158d196
seonki123 <114052655+seonki123@users.noreply.github.com>
2023-09-14 14:55:29 +0900

    num

---
+++ 20230914/20230914/lecture1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/*void main() {
+
+	int num;
+	printf("1. 정수를 입력하세요");
+	scanf_s("%d", &num);
+	printf("2. 입력된 정수는 %d 입니다",num);
+}
+*/
---
+++ 20230914/20230914/lecture3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main() {
+
+	int val_1, val_2, a, b;
+	printf("a=10,b=15¿œ∂ß\n");
+
+	a = 10;
+	b = 15;
+	val_1 = ++a + b--;
+
+	printf("1. val_1=%d,a=%d,b=%d\n", val_1, a, b);
+
+}
